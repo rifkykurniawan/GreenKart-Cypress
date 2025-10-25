9b6d9f638799c8dd45f2888a21e7172d8802b9ae
rifkykurniawan <toettoet.tt@gmail.com>
2025-10-25 18:16:01 +0700

    Update test cases

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,31 @@
+import { homeActions } from '../page-object/page-action/homeActions';
+const data = require('../fixtures/example.json');
+
+
+describe('Home Page', () => {
+    beforeEach(() => {
+      cy.visit(data.URL);
+    });
+    it('Verify chart button is visible', () => {
+      homeActions.clickChartButton();
+      homeActions.isChartImageVisible();
+    }),
+    it('Verify user can add item to cart', () => {
+      homeActions.clickItem1();
+    });
+    it('Verify user can see item count in cart', () => {
+      homeActions.clickItem1();
+      homeActions.getChartCount();
+      homeActions.clickChartButton();
+    });
+    it('Verify chart page is visible', () => {
+      homeActions.clickItem1();
+      homeActions.clickChartButton();
+      homeActions.clickProceedToCheckoutButton();
+      homeActions.isPlaceOrderButtonVisible();
+      homeActions.isPromoCodeInputVisible();
+      homeActions.isPromoCodeApplyButtonVisible();
+      homeActions.isTotalAmountVisible();
+      homeActions.isCartTableVisible();
+    });
+  });
